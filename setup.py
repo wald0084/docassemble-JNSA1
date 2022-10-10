6657b01bdd75cfa4cc310de4d66b0531d880f5ed
Betelihem Waldron <110792525+wald0084@users.noreply.github.com>
2022-10-10 12:26:23 +1030

    Final Prototype

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.JNSA1',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description='# docassemble.JNSA1\n\nA docassemble extension.\n\n## Author\n\nBetelihem Waldron, wald0084@flinders.edu.au\n\n',
+      long_description='# docassemble.JNSA1\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nBetelihem Waldron, wald0084@flinders.edu.au\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Betelihem Waldron',
       author_email='wald0084@flinders.edu.au',
